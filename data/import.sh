76d9be6050a6bf1912d6c83587aaf7fdd5559b8c
Tyler Fisher <tyler@tylerjfisher.com>
2014-08-19 10:45:23 -0400

    the codes. the codes

--- data/import.sh
+++ data/import.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-# unfortunately, csvkit chokes on the dates in this file, so had to save the sheets manually
-#in2csv --sheet "STATES A-F" --no-inference LESO\ Jan\ 2006\ to\ April\ 2014.xlsx > a-f.csv
-#in2csv --sheet "STATES G-M" --no-inference LESO\ Jan\ 2006\ to\ April\ 2014.xlsx > g-m.csv
-#in2csv --sheet "STATES N-S" --no-inference LESO\ Jan\ 2006\ to\ April\ 2014.xlsx > n-s.csv
-#in2csv --sheet "STATES T-W" --no-inference LESO\ Jan\ 2006\ to\ April\ 2014.xlsx > t-w.csv
-
 # clean up dates and strings!
-echo "Run clean.py to generate leso.csv"
-./clean.py
-
-# get the columns we want
-csvcut -c 1,2,4,5,7,8 leso.csv > leso-clean.csv
-
-# csvkit works a-ok for mraps
-echo "Generate mraps.csv"
-in2csv --sheet "cleaned data" --no-inference MRAPS\ by\ County\ as\ of\ 24\ July\ 2014.xlsx > mraps.csv
-
-# make the cost columns the same
-echo "clean cost columns"
-sed -i '' -e 's/Acquisition Cost/Cost/' leso-clean.csv
-sed -i '' -e 's/Unit Cost/Cost/' mraps.csv
-
-# echo "Run merge.py to merge mraps with leso"
-# ./merge.py
-
+# echo "Run clean.py to generate leso.csv"
+# ./clean.py
 
 # setup our database
 echo "Create database"
@@ -35,8 +13,11 @@ psql leso -c "CREATE EXTENSION postgis_topology"
 psql leso -c "SELECT postgis_full_version()"
 
 # get leso csv in the db
-psql leso -c "CREATE TABLE data (STATE char(2), COUNTY varchar, NAME varchar, UNIT varchar, COST varchar, DATE varchar);"
-psql leso -c "COPY data FROM '/Users/tylerfisher/src/leso/data/stacked.csv' DELIMITER ',' CSV;"
+psql leso -c "CREATE TABLE data (STATE char(2), COUNTY varchar, ID varchar, NAME varchar, UNIT varchar, UI varchar, COST varchar, DATE varchar);"
+psql leso -c "COPY data FROM '`pwd`/leso.csv' DELIMITER ',' CSV;"
+
+psql leso -c "CREATE TABLE codes (CODE varchar(16), NAME text, START_DATE varchar, END_DATE varchar, FULL_NAME text, EXCLUDES text, NOTES text, INCLUDES text)"
+psql leso -c "COPY codes FROM '`pwd`/codes.csv' DELIMITER ',' CSV;"
 
 if [ ! -f "./tl_2013_us_county.zip" ]
 then
