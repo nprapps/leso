250f71ca3cb1c99ddf8f6d48e4456bb779ba1ace
David Eads <davideads@gmail.com>
2014-08-20 15:21:28 -0400

    add population census tables

--- data/import.sh
+++ data/import.sh
@@ -32,7 +32,7 @@ echo "Import FIPS crosswalk"
 psql leso -c "CREATE TABLE fips (
   county varchar,
   state varchar,
-  fips integer
+  fips varchar
 );"
 psql leso -c "COPY fips FROM '`pwd`/fips_crosswalk.csv' DELIMITER ',' CSV HEADER;"
 
@@ -52,6 +52,34 @@ psql leso -c "COPY codes FROM '`pwd`/codes.csv' DELIMITER ',' CSV HEADER;"
 
 # De-dupe the supply codes
 psql leso -c "DELETE FROM codes USING codes codes2 WHERE codes.code=codes2.code AND codes.START_DATE > codes2.START_DATE;"
+
+echo "Import ACS 5 year data"
+psql leso -c "CREATE TABLE acs(
+  census_id VARCHAR,
+  fips VARCHAR,
+  place_name VARCHAR,
+  total INTEGER,
+  total_error VARCHAR,
+  white_alone INTEGER,
+  white_alone_error NUMERIC,
+  black_alone INTEGER,
+  black_alone_error NUMERIC,
+  indian_alone INTEGER,
+  indian_alone_error NUMERIC,
+  asian_alone INTEGER,
+  asian_alone_error NUMERIC,
+  hawaiian_alone INTEGER,
+  hawaiian_alone_error NUMERIC,
+  other_race_alone INTEGER,
+  other_race_alone_error NUMERIC,
+  two_or_more_races INTEGER,
+  two_or_more_races_error NUMERIC,
+  two_or_more_races_including INTEGER,
+  two_or_more_races_including_error NUMERIC,
+  two_or_more_races_excluding INTEGER,
+  two_or_more_races_excluding_error NUMERIC
+);"
+PGCLIENTENCODING=LATIN1 psql leso -c "COPY acs FROM '`pwd`/census/acs_12_5yr_b02001.csv' DELIMITER ',' CSV"
 
 echo "Generate unit distribution"
 psql leso -c "COPY (select ui, count(*), sum(quantity) as total_quantity, sum((quantity*acquisition_cost)) as total_cost from data group by ui order by count desc) to '`pwd`/unit_distribution.csv' WITH CSV HEADER;"
@@ -96,6 +124,17 @@ select d.item_name, c.full_name, c.code as federal_supply_code,
   order by d.item_name
 ) to '`pwd`/item_name_distribution.csv' WITH CSV HEADER;"
 
+echo "Generate population table"
+psql leso -c "COPY (
+  select d.state, d.county, 
+    a.total, a.white_alone, a.black_alone, a.indian_alone, a.asian_alone, a.hawaiian_alone, a.other_race_alone, a.two_or_more_races, a.two_or_more_races_including, a.two_or_more_races_excluding,
+    sum((d.quantity * d.acquisition_cost)) as total_cost, (sum((d.quantity * d.acquisition_cost))/a.total) as cost_per_capita
+  from data as d
+  join fips as f on d.state = f.state and d.county = f.county
+  join acs as a on f.fips = a.fips
+  group by d.state, d.county, a.total, a.white_alone, a.black_alone, a.indian_alone, a.asian_alone, a.hawaiian_alone, a.other_race_alone, a.two_or_more_races, a.two_or_more_races_including, a.two_or_more_races_excluding
+) to '`pwd`/cost_by_population.csv' WITH CSV HEADER;"
+
 if [ ! -f "./tl_2013_us_county.zip" ]
 then
   echo "Get county TIGER data"
@@ -110,3 +149,5 @@ echo "Import geo data"
 PGCLIENTENCODING=LATIN1 ogr2ogr -f PostgreSQL PG:dbname=leso tl_2013_us_county/tl_2013_us_county.shp -t_srs EPSG:900913 -nlt multipolygon -nln tl_2013_us_county
 
 # merge with geo data
+
+
