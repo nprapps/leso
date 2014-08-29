1ad7aa60a624a317980720acbd38916084177fb9
David Eads <davideads@gmail.com>
2014-08-29 17:29:16 -0400

    clean up clean.py

--- clean.py
+++ clean.py
@@ -2,8 +2,7 @@
 
 import csv
 import xlrd
-
-from util import make_headers, clean_data
+from util import clean_data
 
 IMPORT_FILE = "src/LESO Jan 2006 to April 2014.xlsx"
 
@@ -11,12 +10,15 @@ if __name__ == "__main__":
     workbook = xlrd.open_workbook(IMPORT_FILE)
     datemode = workbook.datemode
     worksheets = workbook.sheet_names()
-    headers = make_headers(workbook.sheet_by_name(worksheets[0]))
-    headers['federal_supply_class'] = 'federal_supply_class'
-    headers['federal_supply_category'] = 'federal_supply_category'
-    f = open("src/leso.csv", "w")
-    writer = csv.DictWriter(f, fieldnames=headers.values())
-    writer.writeheader()
     for worksheet in worksheets:
         sheet = workbook.sheet_by_name(worksheet)
-        clean_data(sheet, writer, headers, datemode)
+        data = clean_data(sheet, datemode)
+
+    headers = data[0].keys()
+    print(headers)
+
+    f = open("src/leso.csv", "w")
+    writer = csv.DictWriter(f, fieldnames=headers)
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
